9c57a2f51bfa87cd4d44255e78bad4dc21d28cb3
ntsafas <ntsafas@gmail.com>
2025-04-22 17:27:27 +0300

    Change identation to 4 spaces

--- Core/Src/stm32f1xx_it.c
+++ Core/Src/stm32f1xx_it.c
@@ -6,87 +6,87 @@
 /*           Cortex-M3 Processor Interruption and Exception Handlers          */
 /******************************************************************************/
 /**
-  * @brief This function handles Non maskable interrupt.
-  */
+    * @brief This function handles Non maskable interrupt.
+    */
 void NMI_Handler(void)
 {
-   while (1)
-  {
+     while (1)
+    {
 
-  }
+    }
 }
 
 /**
-  * @brief This function handles Hard fault interrupt.
-  */
+    * @brief This function handles Hard fault interrupt.
+    */
 void HardFault_Handler(void)
 {
-  while (1)
-  {
+    while (1)
+    {
 
-  }
+    }
 }
 
 /**
-  * @brief This function handles Memory management fault.
-  */
+    * @brief This function handles Memory management fault.
+    */
 void MemManage_Handler(void)
 {
-  while (1)
-  {
+    while (1)
+    {
 
-  }
+    }
 }
 
 /**
-  * @brief This function handles Prefetch fault, memory access fault.
-  */
+    * @brief This function handles Prefetch fault, memory access fault.
+    */
 void BusFault_Handler(void)
 {
-  while (1)
-  {
+    while (1)
+    {
 
-  }
+    }
 }
 
 /**
-  * @brief This function handles Undefined instruction or illegal state.
-  */
+    * @brief This function handles Undefined instruction or illegal state.
+    */
 void UsageFault_Handler(void)
 {
-  while (1)
-  {
+    while (1)
+    {
 
-  }
+    }
 }
 
 /**
-  * @brief This function handles System service call via SWI instruction.
-  */
+    * @brief This function handles System service call via SWI instruction.
+    */
 void SVC_Handler(void)
 {
 
 }
 
 /**
-  * @brief This function handles Debug monitor.
-  */
+    * @brief This function handles Debug monitor.
+    */
 void DebugMon_Handler(void)
 {
 
 }
 
 /**
-  * @brief This function handles Pendable request for system service.
-  */
+    * @brief This function handles Pendable request for system service.
+    */
 void PendSV_Handler(void)
 {
 
 }
 
 /**
-  * @brief This function handles System tick timer.
-  */
+    * @brief This function handles System tick timer.
+    */
 void SysTick_Handler(void)
 {
 
@@ -100,12 +100,12 @@ void SysTick_Handler(void)
 /******************************************************************************/
 
 /**
-  * @brief This function handles EXTI line[15:10] interrupts.
-  */
+    * @brief This function handles EXTI line[15:10] interrupts.
+    */
 void EXTI15_10_IRQHandler(void)
 {
-  if (LL_EXTI_IsActiveFlag_0_31(LL_EXTI_LINE_13) != RESET)
-  {
-    LL_EXTI_ClearFlag_0_31(LL_EXTI_LINE_13);
-  }
+    if (LL_EXTI_IsActiveFlag_0_31(LL_EXTI_LINE_13) != RESET)
+    {
+        LL_EXTI_ClearFlag_0_31(LL_EXTI_LINE_13);
+    }
 }
